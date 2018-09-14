f0a0b46a3733f4066919dc8fc065baf5883f4f8a
GabrielDgg <gagonzalez@est.frba.utn.edu.ar>
2018-09-14 16:23:27 -0300

    corregi ejercicio 4 frond end

---
+++ FrontEnd/Ejercicio 4/WebApplication2/Scripts/JavaScript.js
@@ -0,0 +1,30 @@
+﻿alert("HOLA");
+var motos = [];
+var BMW = new Object();
+BMW.marca = "BMW";
+BMW.modelo = [];
+BMW.modelo.push("model1");
+BMW.modelo.push("model2");
+motos.push(BMW);
+
+var honda = new Object();
+honda.marca = "honda";
+honda.modelo = [];
+honda.modelo.push("model3");
+honda.modelo.push("model4");
+motos.push(honda);
+
+var json = JSON.stringify(motos) ;
+
+
+var enString = json;
+
+var salida = "";
+
+miObjato = JSON.parse(enString);
+for (i in miObjato) {
+    salida += "Marca=" + miObjato[i].marca + '\n';
+    salida += "Modelo=" + miObjato[i].modelo + '\n';
+}
+
+alert(salida);
